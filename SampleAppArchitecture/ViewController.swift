8ac627ecef536eee9b0375795c575417a866f466
SoniyaGadekar <soniya_gadekar@persistent.com>
2019-09-11 15:22:47 +0530

    Initial Commit

---
+++ SampleAppArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAppArchitecture
+//
+//  Created by  Soniya Gadekar on 11/09/19.
+//  Copyright © 2019  Soniya Gadekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
